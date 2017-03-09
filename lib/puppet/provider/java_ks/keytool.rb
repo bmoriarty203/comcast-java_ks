8e03bb899fe43ce919159144ecff0eeb69901bc0
bmoriarty203 <brian@moriarty.org>
2017-03-09 14:01:00 -0500

    Typo 'deststorepass'

--- lib/puppet/provider/java_ks/keytool.rb
+++ lib/puppet/provider/java_ks/keytool.rb
@@ -76,7 +76,7 @@ Puppet::Type.type(:java_ks).provide(:keytool) do
         '-alias', @resource[:name]
     ]
     cmd << '-trustcacerts' if @resource[:trustcacerts] == :true
-    cmd += [ '-storetype', @resource[:storetype], '-deststoretype',  @resource[:password], '-srcstorepass', @resource[:password], '-noprompt' ] unless @resource[:storetype].nil?
+    cmd += [ '-storetype', @resource[:storetype], '-deststorepass',  @resource[:password], '-srcstorepass', @resource[:password], '-noprompt' ] unless @resource[:storetype].nil?
     cmd += [ '-destkeypass', @resource[:destkeypass] ] unless @resource[:destkeypass].nil?
 
     pwfile = password_file
